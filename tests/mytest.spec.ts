5bb72f59e48bd57c603a40d0ffc8e49c46dd5767
AyanBohara <140262198+AyanBohara@users.noreply.github.com>
2026-09-20 20:28:55 +0545

    First Playwright

---
+++ tests/mytest.spec.ts
@@ -0,0 +1,24 @@
+import { test, expect } from '@playwright/test';
+
+// test("title",()=>{
+
+//   //step1
+//   //step2
+
+// })
+
+//fixture- global variable eg page, browser, context
+
+test(" Verify page title", async ({ page }) => {
+
+  await page.goto("https://demo.openemr.io/a/openemr/interface/login/login.php?site=default")
+
+  let title: string = await page.title();
+  console.log(title);
+
+  await expect(page).toHaveURL(/demo.openemr.io/);
+
+
+
+
+})
