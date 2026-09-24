c1879359fd8c5fd8e2c1e16cf1ee897d62cd1d94
AyanBohara <140262198+AyanBohara@users.noreply.github.com>
2026-09-24 22:08:32 +0545

    test: tested iframe content by using frame locator and content frame

--- tests/iframe.spec.ts
+++ tests/iframe.spec.ts
@@ -11,36 +11,28 @@ test('iframe testing', async ({ page }) => {
 
     await expect(frames.getByText('✅ Ayan | a@gmail.com | editor')).toBeVisible();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 });
+
+test.only("Content frame test", async ({ page }) => {
+    await page.goto('https://practice.rcvacademy.com/iframe');
+    const iframe = page.locator('//*[@id="iframe-frame-1"]');
+    const frame = iframe.contentFrame();
+    await frame.getByPlaceholder('Jane Doe').fill('Zayan');
+    await frame.getByPlaceholder('jane@example.com').fill('Zayan@gmail.com');
+    await frame.locator('#iframe-role-select').selectOption({ value: 'editor' });
+    await frame.getByRole('button', { name: 'Submit' }).click();
+
+
+    const iframea = page.locator('//*[@id="iframe-frame-2"]');
+    const framea = iframea.contentFrame();
+    for (let i = 0; i < 5; i++) {
+        await framea.locator('#iframe-decrement').click();
+    }
+    for (let j = 0; j < 6; j++) {
+        await framea.locator('#iframe-increment').click();
+    }
+
+
+
+
+})
