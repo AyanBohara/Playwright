6997ae91f23822c74825eb04bed58033ebd4c5a5
AyanBohara <140262198+AyanBohara@users.noreply.github.com>
2026-09-25 22:41:42 +0545

    tested: Handling multiple windows

--- tests/fixtureAndContext.spec.ts
+++ tests/fixtureAndContext.spec.ts
@@ -7,3 +7,35 @@ test('Fixture and conetxt demo', async ({ browser }) => {
     const page = await context.newPage();
     await page.goto("https://practice.rcvacademy.com/form-validation");
 })
+
+test.only('Handling multiple windows', async ({ browser }) => {
+
+    //creates a newicognito browser context
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto("https://practice.rcvacademy.com/form-validation");
+
+
+    const [newPage] = await Promise.all([
+        //need to listen if new tab is opening while clicking on page
+        context.waitForEvent("page"), //pening ,fulfiiled or rejected
+        await page.locator("//div[@id='sponsor-tier-platinum']").click()
+
+    ])
+
+    await newPage.locator("//div[@data-component='button']").click();
+    await page.waitForTimeout(5000);
+    await newPage.getByRole('link', { name: 'Sign In' }).click();
+    await page.waitForTimeout(5000);
+    await newPage.getByPlaceholder('Email Address').fill('Ayan');
+    await page.waitForTimeout(5000);
+
+    await newPage.close();
+    await page.waitForTimeout(5000);
+
+    await page.getByPlaceholder('First name').fill('Ayan');
+    await page.waitForTimeout(5000);
+    await page.close();
+
+
+})
