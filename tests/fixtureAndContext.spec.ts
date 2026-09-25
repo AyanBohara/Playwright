455f1b1c3397243b1fd781f4e8d221b56bbda975
AyanBohara <140262198+AyanBohara@users.noreply.github.com>
2026-09-25 21:22:03 +0545

    deep dive into config file

--- tests/fixtureAndContext.spec.ts
+++ tests/fixtureAndContext.spec.ts
@@ -4,5 +4,6 @@ test('Fixture and conetxt demo', async ({ browser }) => {
 
     //creates a newicognito browser context
     const context = await browser.newContext();
-    const page = context.newPage();
+    const page = await context.newPage();
+    await page.goto("https://practice.rcvacademy.com/form-validation");
 })
